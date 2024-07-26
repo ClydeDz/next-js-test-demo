d4ba0260c097a72b356a3e5d0bab18632e1fa695
Clyde D'Souza <clyd94@gmail.com>
2024-07-26 14:23:53 +1200

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+// import styles from "../page.module.css";
+
+export default function Index() {
+  return (
+    // <main className={styles.main}>
+    <h1>Hello, home</h1>
+    // </main>
+  );
+}
